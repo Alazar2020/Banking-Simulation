fc548f42b5dea31cb5f18ecb2acf54929ca78ad7
Alazarf <alazarhailemeskel09@gmail.com>
2022-07-09 14:17:28 -0400

    Javalin removed from class

--- src/main/java/com/alazar/revature/App.java
+++ src/main/java/com/alazar/revature/App.java
@@ -14,7 +14,6 @@ import com.alazar.revature.services.AccountServices;
 import com.alazar.revature.services.AccountServicesimpl;
 import com.alazar.revature.services.ClientServices;
 import com.alazar.revature.services.ClientServicesimpl;
-import io.javalin.Javalin;
 
 public class App {
 
@@ -22,16 +21,8 @@ public class App {
 
 	public static void main(String[] args) {
 
-		Javalin app = Javalin.create();
-		log.info("Javalin Created");
+		
 
-		establishRoutes(app);
-
-		app.start(7000);
-
-		log.info("Javalin Started");
-	}
-
-	private static void establishRoutes(Javalin app) {
+	
 	}
 }
