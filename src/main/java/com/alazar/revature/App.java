7288a9eba5da6863d64b7092799f6ec07bd45491
Alazarf <alazarhailemeskel09@gmail.com>
2022-07-09 23:19:14 -0400

    Main

--- src/main/java/com/alazar/revature/App.java
+++ src/main/java/com/alazar/revature/App.java
@@ -6,11 +6,7 @@ import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
-import pojos.AccountInfo;
-import pojos.User;
-import com.alazar.revature.services.AccountServices;
-import services.AccTypeService;
-import services.UserService;
+
 
 public class App {
 
